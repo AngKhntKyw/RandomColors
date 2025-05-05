0f9315e358ea75eded96914ef6cc52269b185ec0
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-05 16:39:19 +0630

    update

--- RandomColors/UIColor+Ext.swift
+++ RandomColors/UIColor+Ext.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+  
+    static func random() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+    
+}
